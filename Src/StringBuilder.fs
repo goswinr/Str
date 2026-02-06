70313a6d974d34c4f45c0f5b74a9b4e6da33b279
Goswin Rothenthal <goswin@rothenthal.com>
2026-02-06 17:13:19 +0200

    doc string

--- Src/StringBuilder.fs
+++ Src/StringBuilder.fs
@@ -23,22 +23,29 @@ module AutoOpenExtensionsStringBuilder =
 
         // TODO: add overload with length: sb.IndexOf (c:char, from:int, length:int )
 
-        /// Like .IndexOf for strings, returns -1 if not found
+        /// Like list.IndexOf but for StringBuilder, returns -1 if not found
+        /// Throws ArgumentOutOfRangeException if 'from' is out of range
         member sb.IndexOf (c:char, from:int ) : int =
+            if from < 0 then ArgumentOutOfRangeException("from",$"StringBuilder.IndexOf: from ({from}) must be non-negative") |> raise
+            let len = sb.Length
+            if from >= len then ArgumentOutOfRangeException("from",$"StringBuilder.IndexOf: from ({from}) must be less than StringBuilder length ({len})") |> raise
             let rec find i =
-                if   i = sb.Length then -1
-                elif sb.[i] = c    then i
+                if   i = len    then -1
+                elif sb.[i] = c then i
                 else find (i+1)
             find(from)
 
-        /// Like .IndexOf for strings, returns -1 if not found
+        /// Like list.IndexOf but for StringBuilder, returns -1 if not found
         /// Always uses StringComparison.Ordinal
+        /// Throws ArgumentOutOfRangeException if 'from' is out of range
         member sb.IndexOf (t:string, from:int) : int =
             // could in theory be improved be using a rolling hash value
             // see also Array.findArray implementation
             // or https://stackoverflow.com/questions/12261344/fastest-search-method-in-stringbuilder
             let ls = sb.Length
             let lt = t.Length
+            if from < 0 then ArgumentOutOfRangeException("from",$"StringBuilder.IndexOf: from ({from}) must be non-negative") |> raise
+            if from >= ls then ArgumentOutOfRangeException("from",$"StringBuilder.IndexOf: from ({from}) must be less than StringBuilder length ({ls})") |> raise
             //printfn "sb :%d t:%d" ls lt
             let rec find ib it = // index in StringBuilder and index in search string
                 //printfn "Search at ib:%d %c for it:%d %c" ib sb.[ib] it  t.[it]
@@ -49,17 +56,19 @@ module AutoOpenExtensionsStringBuilder =
                 else find (ib+1-it) 0
             find from 0
 
-        /// Like .IndexOf for strings, returns -1 if not found
+        /// Like list.IndexOf but for StringBuilder, returns -1 if not found
         member inline sb.IndexOf (c:char) : int =
             sb.IndexOf(c,0)
 
-        /// Like .IndexOf for strings, returns -1 if not found
+        /// Like list.IndexOf but for StringBuilder, returns -1 if not found
         /// always StringComparison.Ordinal
         member inline sb.IndexOf (t:string) : int =
             sb.IndexOf(t,0)
 
+        /// Checks if StringBuilder contains the given character
         member inline sb.Contains (c:char) : bool =
             sb.IndexOf c <> -1
 
+        /// Checks if StringBuilder contains the given string
         member inline sb.Contains (s:string) : bool =
             sb.IndexOf s <> -1
